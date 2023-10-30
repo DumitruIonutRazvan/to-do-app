3a6f917506b3f22c2235205aba90e013694aa504
DumitruIonutRazvan <dumitruionutrazvan92@gmail.com>
2023-10-31 00:13:55 +0200

    my to-do app with localstorage and color picker for every to-do item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'To-do';
+export class AppComponent implements OnInit {
+  title: string = "to-do"
+  todoItem: string = "";
+  todoItemsArray: any[] =[];
+  todoObj: any = {
+    todoItem: "",
+    color: ""
+  }
+
+  constructor(){}
+  
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem("todoItems");
+    if (localData != null) {
+      this.todoItemsArray = JSON.parse(localData);
+    }
+    
+  }
+
+  onItemAdd(){
+    this.todoItemsArray.push(this.todoObj);
+    localStorage.setItem("todoItems", JSON.stringify(this.todoItemsArray))
+    this.todoObj = {
+      todoItem: "",
+      color:""
+    }
+  }
 }
